a500c846ce4525dc2894dc2632e751a87a36882d
Dennis Hou <dennis@fillr.com>
2017-08-14 16:57:43 +1000

    First Commit

--- UnitConverter/ViewController.m
+++ UnitConverter/ViewController.m
@@ -8,11 +8,42 @@
 
 #import "ViewController.h"
 
+double convertUnitOneToUnitTwo(double unitOneValue){
+    double unitTwoValue;
+    return unitTwoValue = 10 * unitOneValue + 2;
+    
+}
+
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *inputField;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentController;
+@property (weak, nonatomic) IBOutlet UILabel *outputField;
 
 @end
 
 @implementation ViewController
+
+- (IBAction)updateButton:(id)sender {
+    
+    NSMutableString *buffer = [NSMutableString new];
+    
+    double userInput = [self.inputField.text doubleValue];
+    
+    [buffer appendString:@"clicked"];
+
+    if (self.segmentController.selectedSegmentIndex == 0){
+        double unitTwoValue = convertUnitOneToUnitTwo(userInput);
+        [buffer appendString:[@(unitTwoValue) stringValue]];
+    }
+    else if (self.segmentController.selectedSegmentIndex == 1){
+        [buffer appendString:@"unit three"];
+    }
+    else{
+        [buffer appendString:@"unit four"];
+    }
+    self.outputField.text = buffer;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
